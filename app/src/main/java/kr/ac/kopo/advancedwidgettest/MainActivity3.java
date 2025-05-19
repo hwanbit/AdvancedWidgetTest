db6f57f3e1a2d821c624055f7a5e48d7f5927481
Hwanbit <hwanbit@gmail.com>
2025-05-19 14:02:57 +0900

    MultiAutoCompleteTextView

--- app/src/main/java/kr/ac/kopo/advancedwidgettest/MainActivity3.java
+++ app/src/main/java/kr/ac/kopo/advancedwidgettest/MainActivity3.java
@@ -27,9 +27,15 @@ public class MainActivity3 extends AppCompatActivity {
         String[] items = {"AI SOFTWARE", "AISW of KOPO", "AI COMPUTER", "AI DATA", "AI MODEL"};
 
         AutoCompleteTextView auto = findViewById(R.id.auto);
+        // 여러 개의 자동 완성 기능 사용 (컴마(,) 기준)
         MultiAutoCompleteTextView multi = findViewById(R.id.multi);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, items);
         auto.setAdapter(adapter);
+
+
+        MultiAutoCompleteTextView.CommaTokenizer commaToken = new MultiAutoCompleteTextView.CommaTokenizer();
+        multi.setTokenizer(commaToken);
+        multi.setAdapter(adapter);
     }
 }
